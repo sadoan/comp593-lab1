ec6982466975fc37a79c4ed1fb833131ca7dbcdc
Rithik <RITHIK.SADYAN@FLEMINGCOLLEGE.CA>
2022-06-03 20:44:41 -0400

    Revert "modified both script files"
    This reverts commit d7408e04d868ac0590b1f5cc51e60e8685b20a95.

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
-#print lines from Zen of python
 print("Readability counts");
 print("Although practicality beats purity");
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
 #print lines from Zen of python
 print("There should be one-- and preferably only one --obvious way to do it");
+print("Although that way may not be obvious at first unless you're Dutch");
