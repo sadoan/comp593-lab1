bbf10ca0d42679af10367da41ce4e155ba39cdc2
Rithik <RITHIK.SADYAN@FLEMINGCOLLEGE.CA>
2022-06-03 21:09:16 -0400

    part 2
    deleted print statement from script1
    added print statement to script2

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #print lines from Zen of python
 print("There should be one-- and preferably only one --obvious way to do it");
 print("Although that way may not be obvious at first unless you're Dutch");
+print("Although practicality beats purity");
